056ac06aad8dca97934ff342109c29bbc9051c5d
Jade Deu <jade.deu@kainos.com>
2022-09-08 14:40:35 +0100

    amended queries

--- Queries.sql
+++ Queries.sql
@@ -1,14 +1,15 @@
 USE Web_Service_JadeD;
 
 
-SELECT Department, COUNT(DISTINCT(Department))
+SELECT Department, COUNT(Department) AS NumberOfPeople
 FROM Employees 
 GROUP BY Department;
 
 SELECT EmployeeID, Name, (StartingSalary * 0.75) AS GrossPay
 From Employees
-WHERE Department != 'Sales';
-
+WHERE Department != 'Sales'
+UNION
 SELECT Employees.EmployeeID, Name, (Commission * TotalSales / 100) AS GrossPay
 FROM Employees, SalesEmployees
 WHERE Department = 'Sales' AND Employees.EmployeeID = SalesEmployees.EmployeeID;
+
